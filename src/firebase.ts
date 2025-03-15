77d46e49c0d7bb470b83e8aaf276aec920f8f3e3
Dmitri Gornakov <helikeel@gmail.com>
2025-03-15 12:10:56 +0200

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDsa5PfGaPkv-h-UYI95UyvUazNg5t6CcE",
-    authDomain: "sinimae7-b2176.firebaseapp.com",
-    projectId: "sinimae7-b2176",
-    storageBucket: "sinimae7-b2176.firebasestorage.app",
-    messagingSenderId: "678545377321",
-    appId: "1:678545377321:web:3ff33f435c3e947e433438",
-    measurementId: "G-YNLD83K44K"
+    apiKey: "...",
+    authDomain: "...",
+    projectId: "...",
+    storageBucket: "...",
+    messagingSenderId: "...",
+    appId: "...",
+    measurementId: "..."
 };
 
 // Инициализация Firebase
